fd9fa7590bc3cf60e4e0fb91e1d31db77b5fcfdc
broxxxar <narzeg@gmail.com>
2014-12-16 22:42:05 +0200

    added final bounciness

---
+++ Assets/Script/BallBounceUp2.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+public var jumpHeight = 8;
+private var jumping = false;
+
+function Update () 
+{
+
+if (Input.GetKeyDown(KeyCode.Space) && jumping == false)
+{
+	rigidbody2D.velocity.y = jumpHeight;
+}
+
+	jumping = true;
+
+}
+
+function OnCollisionStay2D () 
+{
+	jumping = false;
+}
