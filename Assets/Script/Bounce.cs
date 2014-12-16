75d3071233950d130f30caca5e6dde62166c8e7a
broxxxar <narzeg@gmail.com>
2014-12-16 18:09:49 +0200

    INSERTING MY BIG AND HARD BALL

---
+++ Assets/Script/Bounce.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+
+	bool bouncing = false;
+
+	void Start () 
+	{
+		bouncing = false;
+
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+}
