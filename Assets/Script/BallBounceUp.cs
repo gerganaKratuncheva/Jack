3e0b51baab25ae67ce29e5696a56d8fb66f4c278
broxxxar <narzeg@gmail.com>
2014-12-16 20:34:31 +0200

    aded bounnce script(PH),background aswell

---
+++ Assets/Script/BallBounceUp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBounceUp : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		       if (Input.GetKeyDown ("space")){
+			                 transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+			       } 
+}
+}
