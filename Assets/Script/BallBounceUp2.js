15c3fdb4602ddd1c6bd8cce583b9fb74e1b04bc8
broxxxar <narzeg@gmail.com>
2014-12-17 03:50:47 +0200

    changes
    aded resources folder, adin to the bounce script(unused changes as of
    sync)

--- Assets/Script/BallBounceUp2.js
+++ Assets/Script/BallBounceUp2.js
@@ -3,6 +3,13 @@
 public var jumpHeight = 20;
 private var jumping = false;
 public var bounceCounter =-1;
+var sprite : SpriteRenderer;
+
+/*function Start ()
+{
+	
+    sprite = GetComponent(SpriteRenderer);
+}*/
 
 
 function Update () 
@@ -24,5 +31,14 @@ function OnCollisionStay2D ()
 
 function OnCollisionEnter2D ()
 {
+	//THIS SHOULD BE TO ANIMATE THE "SQUISH " OF THE BALL AT LANDING -> NOT WORKING CORRECTLY
+	//ALSO GOES FREEZES THE PC FOR LIKE 2 SEC
+	//sprite.sprite = Resources.Load("KrasioBallFrame2", typeof(Sprite));
 	bounceCounter	++;
 }
+
+/*function OnCollisionExit2D ()
+{
+	//HERE THE BALL SHOULD COME BACK TO NORMAL
+	sprite.sprite = Resources.Load("KrasioBall", typeof(Sprite));
+}*/
